38fe86718be67b44c153982491c9b74adf5a47cf
inoue0426 <sekihan_tennis@yahoo.co.jp>
2022-08-14 18:43:32 -0500

    add data extraction for training

---
+++ code/get_data_for_training.py
@@ -0,0 +1,102 @@
+import networkx as nx
+import numpy as np
+import pandas as pd
+from joblib import Parallel, delayed
+from rdkit import Chem
+from rdkit.Chem import AllChem, rdFingerprintGenerator
+
+train = pd.read_csv(
+    "../DrugCell/data_rcellminer/train_rcell_wo_other.txt",
+    header=None,
+    sep="\t",
+)
+
+
+pd.DataFrame(set(train[1])).reset_index().to_csv(
+    "../DrugCell/data_rcellminer/drug2ind.txt",
+    sep="\t",
+    header=None,
+    index=None,
+)
+
+mfp = np.array(
+    [
+        np.array(
+            AllChem.GetMorganFingerprintAsBitVect(
+                Chem.MolFromSmiles(i), useChirality=True, radius=2, nBits=2048
+            )
+        )
+        for i in set(train[1])
+    ]
+)
+
+
+pd.DataFrame(mfp).to_csv(
+    "../DrugCell/data_rcellminer/drug2fingerprint.csv",
+    sep=",",
+    header=None,
+    index=None,
+)
+
+
+pd.DataFrame(set(train[0])).reset_index().to_csv(
+    "../DrugCell/data_rcellminer/cell2ind.txt",
+    header=None,
+    index=None,
+    sep="\t",
+)
+
+
+cell2ind = pd.read_csv("../DrugCell/data/cell2ind.txt", header=None, sep="\t")
+
+
+t = pd.read_csv("../DrugCell/data_rcellminer/cell2ind.txt", header=None, sep="\t")
+
+
+cell2mut = (
+    pd.read_csv(
+        "../DrugCell/data/cell2mutation.txt",
+        header=None,
+    )
+    .loc[[cell2ind[cell2ind[1] == i].index[0] for i in t[1]]]
+    .reset_index(drop=True)
+)
+
+
+none_zero_cols = list(np.sum(cell2mut) != 0)
+
+
+cell2mut = cell2mut.loc[:, none_zero_cols]
+cell2mut = cell2mut.T.reset_index(drop=True).T
+cell2mut.to_csv(
+    "../DrugCell/data_rcellminer/cell2mut.txt",
+    header=None,
+    #     sep='\t',
+    index=None,
+)
+
+
+gene2ind = pd.read_csv("../DrugCell/data/gene2ind.txt", header=None, sep="\t")
+gene2ind = gene2ind.loc[none_zero_cols]
+pd.DataFrame(list(gene2ind[1])).to_csv(
+    "../DrugCell/data_rcellminer/gene2ind.txt", header=None, sep="\t"
+)
+
+
+graph = pd.read_csv("../DrugCell/data/drugcell_ont.txt", header=None, sep="\t")
+
+
+gene = graph[graph[2] == "gene"]
+
+
+g = pd.DataFrame()
+for i in gene2ind[1]:
+    g = pd.concat([g, gene[gene[1] == i]])
+
+
+go = pd.concat([graph[graph[2] == "default"], g])
+
+
+go.reset_index(drop=True).to_csv(
+    "../DrugCell/data_rcellminer/go.txt", header=None, sep="\t", index=None
+)
---
+++ code/get_data_with_class_label.py
@@ -0,0 +1,105 @@
+import random as rd
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+# Create PubChem_id list
+pubchem_id = pd.read_csv(
+    "../DrugCell/data_rcellminer/pubchem_id_by_nsc.csv", index_col=0
+).dropna()
+pubchem_id
+
+
+smiles = pd.read_table(
+    "../DrugCell/data_rcellminer/SMILES_from_PubchemID.txt", header=None
+)
+smiles.index = pubchem_id.index
+smiles = smiles.drop(0, axis=1)
+smiles.columns = ["smiles"]
+smiles = smiles.to_dict()["smiles"]
+
+
+nci60Act = pd.read_csv("../data/nci60Act_ccle.csv", index_col=0)
+cell2ind = list(pd.read_table("../DrugCell/data/cell2ind.txt", header=None)[1])
+nci60Act = nci60Act[list(set(cell2ind) & set(nci60Act.columns))]
+
+
+base = pd.DataFrame()
+for i in nci60Act.columns:
+    tmp = nci60Act[i].reset_index().dropna()
+    tmp.columns = ["nsc", "drug_response"]
+    tmp["cell_line"] = [i] * len(tmp)
+    base = pd.concat([base, tmp])
+
+
+class_nsc = pd.read_csv(
+    "../DrugCell/data_rcellminer/class_by_nsc.csv",
+)
+
+
+class_nsc["MECHANISM"].value_counts()
+
+
+nsc_list = set(class_nsc["NSC"])
+class_nsc = {class_nsc["NSC"][i]: class_nsc["MECHANISM"][i] for i in class_nsc.index}
+
+
+base["label"] = [class_nsc[i] if i in nsc_list else None for i in base["nsc"]]
+base = base[base["label"] != "Other"].reset_index(drop=True).drop("label", axis=1)
+base["smiles"] = [smiles[i] if i in smiles.keys() else None for i in base.nsc]
+base = base.dropna()
+base = base.reset_index(drop=True)
+base = base[["cell_line", "smiles", "drug_response"]]
+
+
+indexes = list(base.index)
+rd.Random(42).shuffle(indexes)
+
+
+test = indexes[round(len(indexes) * 0.8) :]
+tmp = indexes[: round(len(indexes) * 0.8)]
+val = tmp[: len(test)]
+train = tmp[len(test) :]
+
+
+train = base.iloc[train]
+test = base.iloc[test]
+val = base.iloc[val]
+
+
+df = pd.merge(
+    pd.merge(
+        pd.DataFrame(train["cell_line"].value_counts()),
+        pd.DataFrame(val["cell_line"].value_counts()),
+        left_index=True,
+        right_index=True,
+    ),
+    pd.DataFrame(test["cell_line"].value_counts()),
+    left_index=True,
+    right_index=True,
+)
+df.columns = ["train", "val", "test"]
+df.loc["total"] = np.sum(df, axis=0)
+
+
+test.to_csv(
+    "../DrugCell/data_rcellminer/test_rcell_wo_other.txt",
+    sep="\t",
+    header=None,
+    index=None,
+)
+
+val.to_csv(
+    "../DrugCell/data_rcellminer/val_rcell_wo_other.txt",
+    sep="\t",
+    header=None,
+    index=None,
+)
+
+train.to_csv(
+    "../DrugCell/data_rcellminer/train_rcell_wo_other.txt",
+    sep="\t",
+    header=None,
+    index=None,
+)
