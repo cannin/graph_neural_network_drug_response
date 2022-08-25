47d4f1b588ea3e5d25ec9922a61b9f915bf9cf6b
Yoshitaka Inoue <8393063+inoue0426@users.noreply.github.com>
2022-08-25 16:23:12 -0500

    Update get_data_for_training.py

--- code/get_data_for_training.py
+++ code/get_data_for_training.py
@@ -4,10 +4,10 @@ import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
-train = pd.read_csv("../DrugCell/data_rcellminer/train_DNA.txt", header=None, sep="\t")
+train = pd.read_csv("DrugCell/data_rcellminer/train_DNA.txt", header=None, sep="\t")
 
 pd.DataFrame(set(train[1])).reset_index().to_csv(
-    "../DrugCell/data_rcellminer/drug2ind.txt",
+    "DrugCell/data_rcellminer/drug2ind.txt",
     sep="\t",
     header=None,
     index=None,
@@ -25,26 +25,26 @@ mfp = np.array(
 )
 
 pd.DataFrame(mfp).to_csv(
-    "../DrugCell/data_rcellminer/drug2fingerprint.csv",
+    "DrugCell/data_rcellminer/drug2fingerprint.csv",
     sep=",",
     header=None,
     index=None,
 )
 
 pd.DataFrame(set(train[0])).reset_index().to_csv(
-    "../DrugCell/data_rcellminer/cell2ind.txt",
+    "DrugCell/data_rcellminer/cell2ind.txt",
     header=None,
     index=None,
     sep="\t",
 )
 
-cell2ind = pd.read_csv("../DrugCell/data/cell2ind.txt", header=None, sep="\t")
+cell2ind = pd.read_csv("DrugCell/data/cell2ind.txt", header=None, sep="\t")
 
-t = pd.read_csv("../DrugCell/data_rcellminer/cell2ind.txt", header=None, sep="\t")
+t = pd.read_csv("DrugCell/data_rcellminer/cell2ind.txt", header=None, sep="\t")
 
 cell2mut = (
     pd.read_csv(
-        "../DrugCell/data/cell2mutation.txt",
+        "DrugCell/data/cell2mutation.txt",
         header=None,
     )
     .loc[[int(cell2ind[cell2ind[1] == i][0]) for i in t[1]]]
@@ -55,17 +55,17 @@ none_zero_cols = list(np.sum(cell2mut) != 0)
 cell2mut = cell2mut.loc[:, none_zero_cols]
 cell2mut = cell2mut.T.reset_index(drop=True).T
 cell2mut.to_csv(
-    "../DrugCell/data_rcellminer/cell2mut.txt",
+    "DrugCell/data_rcellminer/cell2mut.txt",
     header=None,
     index=None,
 )
 
-gene2ind = pd.read_csv("../DrugCell/data/gene2ind.txt", header=None, sep="\t")
+gene2ind = pd.read_csv("DrugCell/data/gene2ind.txt", header=None, sep="\t")
 gene2ind = gene2ind.loc[none_zero_cols]
 gene2ind = pd.DataFrame(list(gene2ind[1]))
-gene2ind.to_csv("../DrugCell/data_rcellminer/gene2ind.txt", header=None, sep="\t")
+gene2ind.to_csv("DrugCell/data_rcellminer/gene2ind.txt", header=None, sep="\t")
 
-graph = pd.read_csv("../DrugCell/data/drugcell_ont.txt", header=None, sep="\t")
+graph = pd.read_csv("DrugCell/data/drugcell_ont.txt", header=None, sep="\t")
 gene = graph[graph[2] == "gene"]
 
 g = pd.DataFrame()
@@ -75,5 +75,5 @@ for i in gene2ind[0]:
 go = pd.concat([graph[graph[2] == "default"], g])
 
 go.reset_index(drop=True).to_csv(
-    "../DrugCell/data_rcellminer/go.txt", header=None, sep="\t", index=None
+    "DrugCell/data_rcellminer/go.txt", header=None, sep="\t", index=None
 )
