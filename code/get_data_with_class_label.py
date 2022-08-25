5ba414983a11716324dd5fef0a5ddf1f11806a5d
Yoshitaka Inoue <8393063+inoue0426@users.noreply.github.com>
2022-08-25 15:58:56 -0500

    Update get_data_with_class_label.py

--- code/get_data_with_class_label.py
+++ code/get_data_with_class_label.py
@@ -4,95 +4,68 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-# get pubchem id and nsc list
-pubchem_id = pd.read_csv(
-    "DrugCell/data_rcellminer/pubchem_id_by_nsc.csv", index_col=0
-).dropna()
+"""
 
-# get smiles and pubchem id list
-smiles = pd.read_table(
-    "DrugCell/data_rcellminer/SMILES_from_PubchemID.txt", header=None
-)
-smiles.index = pubchem_id.index
-smiles = smiles.drop(0, axis=1)
-smiles.columns = ["smiles"]
-smiles = smiles.to_dict()["smiles"]
+This script is used to get the data with class label.
+We just choose the data with class label DNA.
 
-# get drug name and nsc list
-nci60Act = pd.read_csv("data/nci60Act_ccle.csv", index_col=0)
-cell2ind = list(pd.read_table("DrugCell/data/cell2ind.txt", header=None)[1])
+"""
+
+# Read the data
+nci60Act = pd.read_csv("../data/nci60Act_ccle.csv", index_col=0)
+cell2ind = list(pd.read_table("../DrugCell/data/cell2ind.txt", header=None)[1])
 nci60Act = nci60Act[list(set(cell2ind) & set(nci60Act.columns))]
+nci60Act
 
-# create base dataframe like test data
+# Make the data format for DrugCell
 base = pd.DataFrame()
 for i in nci60Act.columns:
     tmp = nci60Act[i].reset_index().dropna()
-    tmp.columns = ["nsc", "drug_response"]
+    tmp.columns = ["NSC", "drug_response"]
     tmp["cell_line"] = [i] * len(tmp)
     base = pd.concat([base, tmp])
-
-# get class data by nsc
-class_nsc = pd.read_csv(
-    "DrugCell/data_rcellminer/class_by_nsc.csv",
-)
-
-# create dict for class by nsc
-nsc_list = set(class_nsc["NSC"])
-class_nsc = {class_nsc["NSC"][i]: class_nsc["MECHANISM"][i] for i in class_nsc.index}
-
-# choose data from class
-base["label"] = [class_nsc[i] if i in nsc_list else None for i in base["nsc"]]
-base = base[base["label"] != "Other"].reset_index(drop=True).drop("label", axis=1)
-base["smiles"] = [smiles[i] if i in smiles.keys() else None for i in base.nsc]
-base = base.dropna()
 base = base.reset_index(drop=True)
-base = base[["cell_line", "smiles", "drug_response"]]
 
-# random shuffle to get train, val, and test data
-indexes = list(base.index)
+# Merge class and SMILES on NSC
+class_nsc = pd.read_csv("../DrugCell/data_rcellminer/class_by_nsc.csv")
+base_label = base.merge(class_nsc, on="NSC")
+smiles = pd.read_csv("../data/nsc_cid_smiles.csv")[["NSC", "SMILES"]]
+base_smiles = base_label.merge(smiles, on="NSC").drop("NSC", axis=1)
+
+# Choose the data with class label DNA
+df_dna = base_smiles[base_smiles["MECHANISM"] == "DNA"].reset_index(drop=True)[
+    ["cell_line", "SMILES", "drug_response"]
+]
+indexes = list(df_dna.index)
 rd.Random(42).shuffle(indexes)
 
+# Randomly split the data into train, valid and test
 test = indexes[round(len(indexes) * 0.8) :]
 tmp = indexes[: round(len(indexes) * 0.8)]
 val = tmp[: len(test)]
 train = tmp[len(test) :]
 
-train = base.iloc[train]
-test = base.iloc[test]
-val = base.iloc[val]
+train = df_dna.iloc[train]
+test = df_dna.iloc[test]
+val = df_dna.iloc[val]
 
-
-df = pd.merge(
-    pd.merge(
-        pd.DataFrame(train["cell_line"].value_counts()),
-        pd.DataFrame(val["cell_line"].value_counts()),
-        left_index=True,
-        right_index=True,
-    ),
-    pd.DataFrame(test["cell_line"].value_counts()),
-    left_index=True,
-    right_index=True,
-)
-df.columns = ["train", "val", "test"]
-df.loc["total"] = np.sum(df, axis=0)
-
-# save data
+# Save the data
 test.to_csv(
-    "DrugCell/data_rcellminer/test_rcell_wo_other.txt",
+    "../DrugCell/data_rcellminer/test_DNA.txt",
     sep="\t",
     header=None,
     index=None,
 )
 
 val.to_csv(
-    "DrugCell/data_rcellminer/val_rcell_wo_other.txt",
+    "../DrugCell/data_rcellminer/val_DNA.txt",
     sep="\t",
     header=None,
     index=None,
 )
 
 train.to_csv(
-    "DrugCell/data_rcellminer/train_rcell_wo_other.txt",
+    "../DrugCell/data_rcellminer/train_DNA.txt",
     sep="\t",
     header=None,
     index=None,
