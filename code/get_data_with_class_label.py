5ea7a17003108deda8c657d3bb55e92401484791
Yoshitaka Inoue <8393063+inoue0426@users.noreply.github.com>
2022-08-25 16:23:47 -0500

    Update get_data_with_class_label.py

--- code/get_data_with_class_label.py
+++ code/get_data_with_class_label.py
@@ -13,7 +13,7 @@ We just choose the data with class label DNA.
 
 # Read the data
 nci60Act = pd.read_csv("../data/nci60Act_ccle.csv", index_col=0)
-cell2ind = list(pd.read_table("../DrugCell/data/cell2ind.txt", header=None)[1])
+cell2ind = list(pd.read_table("DrugCell/data/cell2ind.txt", header=None)[1])
 nci60Act = nci60Act[list(set(cell2ind) & set(nci60Act.columns))]
 nci60Act
 
@@ -27,7 +27,7 @@ for i in nci60Act.columns:
 base = base.reset_index(drop=True)
 
 # Merge class and SMILES on NSC
-class_nsc = pd.read_csv("../DrugCell/data_rcellminer/class_by_nsc.csv")
+class_nsc = pd.read_csv("DrugCell/data_rcellminer/class_by_nsc.csv")
 base_label = base.merge(class_nsc, on="NSC")
 smiles = pd.read_csv("../data/nsc_cid_smiles.csv")[["NSC", "SMILES"]]
 base_smiles = base_label.merge(smiles, on="NSC").drop("NSC", axis=1)
@@ -51,21 +51,21 @@ val = df_dna.iloc[val]
 
 # Save the data
 test.to_csv(
-    "../DrugCell/data_rcellminer/test_DNA.txt",
+    "DrugCell/data_rcellminer/test_DNA.txt",
     sep="\t",
     header=None,
     index=None,
 )
 
 val.to_csv(
-    "../DrugCell/data_rcellminer/val_DNA.txt",
+    "DrugCell/data_rcellminer/val_DNA.txt",
     sep="\t",
     header=None,
     index=None,
 )
 
 train.to_csv(
-    "../DrugCell/data_rcellminer/train_DNA.txt",
+    "DrugCell/data_rcellminer/train_DNA.txt",
     sep="\t",
     header=None,
     index=None,
